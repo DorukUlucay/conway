d4cc520a1a1d66e9390a6f92b5b19a5b10b7bac0
DorukUlucay <doruk36@yahoo.com>
2016-06-10 17:48:04 +0300

    add the lib and the console app.

---
+++ conway/Conway.cs
@@ -0,0 +1,102 @@
+﻿namespace conway
+{
+    public class Conway
+    {
+        int _size;
+        int[,] world = null;
+        int[,] newWorld = null;
+
+        public Conway(int size)
+        {
+            _size = size;
+            world = new int[_size, _size];
+            newWorld = new int[_size, _size];
+
+            //to be moved
+            //glider
+            world[6, 4] = 1;
+            world[7, 5] = 1;
+            world[5, 6] = 1;
+            world[6, 6] = 1;
+            world[7, 6] = 1;
+        }
+
+        public int[,] Evolve()
+        {
+            newWorld = new int[_size, _size];
+
+            for (int x = 0; x < _size; x++)
+            {
+                for (int y = 0; y < _size; y++)
+                {
+                    int neighborCount = getAliveNeighborCount(x, y);
+
+                    if (world[x, y] == 0)
+                    {
+                        if (neighborCount == 3)
+                        {
+                            newWorld[x, y] = 1;
+                        }
+                        else
+                        {
+                            newWorld[x, y] = world[x, y];
+                        }
+                    }
+                    else
+                    {
+                        if (neighborCount < 2)
+                        {
+                            newWorld[x, y] = 0;
+                        }
+                        else if (neighborCount > 3)
+                        {
+                            newWorld[x, y] = 0;
+                        }
+                        else
+                        {
+                            newWorld[x, y] = world[x, y];
+                        }
+                    }
+                }
+            }
+
+            world = newWorld;
+            return world;
+        }
+
+        bool isOutOfBounds(int x, int y)
+        {
+            if (x >= _size || x < 0 || y >= _size || y < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        int getAliveNeighborCount(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (!isOutOfBounds(x + i, y + j))
+                    {
+                        if (i == 0 && j == 0)
+                        {
+
+                        }
+                        else
+                        {
+                            if (world[x + i, y + j] == 1)
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ conway/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace conway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 20;
+            int speed = 500;
+            Conway conway = new Conway(size);
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(speed);
+                var world = conway.Evolve();
+                print(world);
+            }
+        }
+
+        static void print(int[,] world)
+        {
+            Console.WriteLine();
+            for (int x = 0; x < world.GetLength(0); x++)
+            {
+                string r = "";
+
+                for (int y = 0; y < world.GetLength(1); y++)
+                {
+                    r += " " + world[x, y].ToString();
+                }
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
